6f699db8e448b761985855f481e15531ce6e9b79
Punio3 <czlapset@wp.pl>
2024-07-16 22:09:24 +0200

    Added display all pieces

--- Chess/ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
+++ Chess/ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3ff957f18129757b4e725b35a7e37f3f0a51a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
