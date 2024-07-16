e516a05ce920fd27381642c5daa525ecfc8c38a6
Punio3 <czlapset@wp.pl>
2024-07-16 22:09:24 +0200

    Added display all pieces

--- Chess/ChessUI/MainWindow.xaml.cs
+++ Chess/ChessUI/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Chesslogic;
 
 namespace ChessUI
 {
@@ -16,9 +17,41 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+
+        private GameState gameState;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            for(int k = 0; k < 8; k++)
+            {
+                for(int i=0;i<8;i++)
+                {
+                    Image image = new Image();
+                    pieceImages[k,i] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for(int k=0; k<8; k++)
+            {
+                for(int i = 0; i < 8; i++)
+                {
+                    Piece piece = board[k, i];
+                    pieceImages[k, i].Source = Images.GetImage(piece);
+                }
+            }
         }
     }
 }
