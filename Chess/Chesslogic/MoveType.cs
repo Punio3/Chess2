0ae606b8403a1699f0c8d568ee9364a6df2c7879
Punio3 <czlapset@wp.pl>
2025-02-25 19:05:33 +0100

    dodanie pliku

---
+++ Chess/Chesslogic/MoveType.cs
@@ -0,0 +1,12 @@
+﻿namespace Chesslogic
+{
+    public enum MoveType
+    {
+        Normal,
+        CastleKS,
+        CastleQS,
+        DoublePawn,
+        EnPassant,
+        PawnPromotion
+    }
+}
