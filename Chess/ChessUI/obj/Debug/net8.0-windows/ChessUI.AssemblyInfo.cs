3c23d974f28e3321287912ab7f30c773d424b67c
Punio3 <czlapset@wp.pl>
2025-02-25 18:50:45 +0100

    Zatwierdzenie biezacej wersji

--- Chess/ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
+++ Chess/ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafc18126b35915e0369a788e2bb52599fa49c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Chess/Chesslogic/obj/Debug/net8.0/Chesslogic.AssemblyInfo.cs
+++ Chess/Chesslogic/obj/Debug/net8.0/Chesslogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chesslogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafc18126b35915e0369a788e2bb52599fa49c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chesslogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chesslogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
